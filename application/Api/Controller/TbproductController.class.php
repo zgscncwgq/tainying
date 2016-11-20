8bae3481f953428243758719808b818ba60ffde3
zhang <zharty@163.com>
2016-11-20 16:17:52 +0800

    淘宝数据

--- application/Api/Controller/TbproductController.class.php
+++ application/Api/Controller/TbproductController.class.php
@@ -74,6 +74,64 @@ class TbproductController extends Controller{
     	$data=M('taobaoproduct')->limit(80)->select();
     	$this->ajaxreturn($data);
     }
+	
+	public function addonelink(){
+		$result['status']=0;
+		$result['msg']="数据信息";
+		if(!I("proid")){
+			$this->ajaxreturn($result);
+			exit();
+		}
+		include SITE_PATH."simplewind/Core/Library/Taobao/TopSdk.php";
+		$numIids=I("proid");
+		$c = new \TopClient;
+		$c->appkey = $this->appkey;
+		$c->secretKey = $this->secret;
+
+		$req = new \TbkItemInfoGetRequest;
+		$req->setFields("num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,nick,seller_id,volume,tk_rate");
+		
+		$req->setPlatform("1");
+		$req->setNumIids($numIids);
+		$resp = $c->execute($req);
+		$suldata=json_encode($resp);
+		$tdata=json_decode($suldata,true);
+
+		
+		if($suldata){
+				$vl=$tdata["results"]["n_tbk_item"];
+
+				$paremat["num_iid"]=$vl["num_iid"];//商品ID
+				$paremat["item_url"]=$vl["item_url"];//商品地址
+
+				$paremat["pict_url"]=$vl["pict_url"];//商品主图
+				$paremat["provcity"]=$vl["provcity"];//宝贝所在地
+				$paremat["reserve_price"]=$vl["reserve_price"];//	
+
+				$paremat["title"]=$vl["title"];//商品标题
+				$paremat["user_type"]=$vl["user_type"];//卖家类型，0表示集市，1表示商城
+				$paremat["zk_final_price"]=$vl["zk_final_price"];//商品折扣价格
+				$paremat["user_type"]=$vl["user_type"];//商品折扣价格
+
+				$paremat['nick']=$vl['nick'];//卖家昵称
+				$paremat['seller_id']=$vl['seller_id'];//卖家id
+
+				$paremat['volume']=$vl['volume'];//30天销量
+				$paremat['tk_rate']=$vl['tk_rate'];
+
+				$paremat['uptime']=time();
+		}
+		
+		$paremat['proid']=I("proid");
+		$paremat['crtime']=time();		
+		$sul=M('taobaoproduct')->where("proid='%s'",$numIids)->count();
+		if($sul<=0){
+			$sul=M('taobaoproduct')->add($paremat);
+			$this->ajaxreturn($paremat);
+		}else{
+			$this->ajaxreturn($paremat);
+		}
+	}
 
     public function getTbproduct(){
     	include SITE_PATH."simplewind/Core/Library/Taobao/TopSdk.php";
@@ -120,8 +178,7 @@ class TbproductController extends Controller{
 			}
 			
 		}
-		dump($tdata);
-
+		$this->ajaxreturn($tdata);
     }
 }
 
